655083b5a852da9ceb1d419830f6f82bb6750657
Robert Portier <robert.portier@apparanto.com>
2024-07-16 09:35:14 +0200

    chore: Update OCI cart route to use 'data_array' instead of 'form' for payload

--- app/api/oci/cart/[punchoutId]/route.ts
+++ app/api/oci/cart/[punchoutId]/route.ts
@@ -19,7 +19,7 @@ export async function POST(request: NextRequest, { params: { punchoutId } }: { p
     const result = {
         'url': `${process.env.OCI_FORM_ACTION_URL}/${punchoutId}`,
         'method': `POST`,
-        'form': [
+        'data_array': [
             {
                 'key': 'orderId',
                 'value': punchoutId
@@ -30,5 +30,45 @@ export async function POST(request: NextRequest, { params: { punchoutId } }: { p
             }
         ]
     }
-    return new NextResponse(JSON.stringify(result), { status: 201 });
+    const result2 = {
+        "message": null,
+        //"url": "https://punchout.cloud/oci/cart-viewer",
+        'url': `${process.env.OCI_FORM_ACTION_URL}/${punchoutId}`,
+        "data_array": [
+            { "key": "NEW_ITEM-CURRENCY[2]", "value": "EUR", },
+            { "key": "NEW_ITEM-QUANTITY[3]", "value": "1" },
+            { "key": "NEW_ITEM-VENDORMAT[2]", "value": "" },
+            { "key": "NEW_ITEM-DESCRIPTION[3]", "value": "Plate,filler" },
+            { "key": "NEW_ITEM-PRICE[2]", "value": "0" },
+            { "key": "NEW_ITEM-PRICEUNIT[3]", "value": "1" },
+            { "key": "NEW_ITEM-VENDORMAT[3]", "value": "" },
+            { "key": "NEW_ITEM-QUANTITY[2]", "value": "12" },
+            { "key": "NEW_ITEM-VENDORMAT[1]", "value": "" },
+            { "key": "NEW_ITEM-DESCRIPTION[1]", "value": "Gearbox, 6S1611BO+INT3" },
+            { "key": "NEW_ITEM-QUANTITY[1]", "value": "1" },
+            { "key": "NEW_ITEM-PRICEUNIT[1]", "value": "1" },
+            { "key": "NEW_ITEM-MATGROUP[1]", "value": "" },
+            { "key": "NEW_ITEM-PRICE[1]", "value": "0" },
+            { "key": "NEW_ITEM-MANUFACTMAT[2]", "value": "" },
+            { "key": "NEW_ITEM-LEADTIME[3]", "value": "" },
+            { "key": "NEW_ITEM-LEADTIME[1]", "value": "" },
+            { "key": "NEW_ITEM-CURRENCY[1]", "value": "EUR" },
+            { "key": "NEW_ITEM-LEADTIME[2]", "value": "" },
+            { "key": "NEW_ITEM-UNIT[1]", "value": "EA" },
+            { "key": "NEW_ITEM-MANUFACTMAT[1]", "value": "" },
+            { "key": "NEW_ITEM-MATGROUP[2]", "value": "" },
+            { "key": "NEW_ITEM-UNIT[3]", "value": "EA" },
+            { "key": "NEW_ITEM-MANUFACTMAT[3]", "value": "" },
+            { "key": "NEW_ITEM-DESCRIPTION[2]", "value": "Bolt,flange, M10X70 DAF9257" },
+            { "key": "NEW_ITEM-MATGROUP[3]", "value": "" },
+            { "key": "NEW_ITEM-PRICE[3]", "value": "0" },
+            { "key": "NEW_ITEM-UNIT[2]", "value": "EA" },
+            { "key": "NEW_ITEM-CURRENCY[3]", "value": "EUR" },
+            { "key": "NEW_ITEM-PRICEUNIT[2]", "value": "1" }
+        ],
+        "method": null,
+        "action": null
+    }
+
+    return new NextResponse(JSON.stringify(result2), { status: 201 });
 }
--- app/api/oci/punchoutFormAction/[sessionId]/route.ts
+++ app/api/oci/punchoutFormAction/[sessionId]/route.ts
@@ -3,5 +3,5 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest, { params: { sessionId } }: { params: { sessionId: string } }) {
     const formData = await request.formData();
     console.log(formData);
-    return new NextResponse('OK');
+    return new NextResponse(`${JSON.stringify(Object.fromEntries(formData))}}`);
 }
