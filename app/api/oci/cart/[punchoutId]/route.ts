af4db0abb7feacdc9ca21e7b1fe8852eb1e76454
Robert Portier <robert.portier@apparanto.com>
2024-07-16 10:20:21 +0200

    feat: Add support for adding items to cart in OCI punchout

--- app/api/oci/cart/[punchoutId]/route.ts
+++ app/api/oci/cart/[punchoutId]/route.ts
@@ -30,42 +30,36 @@ export async function POST(request: NextRequest, { params: { punchoutId } }: { p
             }
         ]
     }
+    const items = [];
+    const articles = json['cart']['MendixVDL']['orderLines'];
+    for (let i = 0; i < articles.length; i++) {
+        const article = articles[i];
+        items.push({
+            'key': `NEW_ITEM-VENDORMAT[${i + 1}]`,
+            'value': article['articleNr']
+        })
+        items.push({
+            'key': `NEW_ITEM-DESCRIPTION[${i + 1}]`,
+            'value': article['description']
+        });
+        items.push({
+            'key': `NEW_ITEM-QUANTITY[${i + 1}]`,
+            'value': article['quantity']
+        });
+        items.push({
+            'key': `NEW_ITEM-CURRENCY[${i + 1}]`,
+            'value': 'EUR'
+        });
+        items.push({
+            'key': `NEW_ITEM-UNIT[${i + 1}]`,
+            'value': 'EA'
+        });
+    }
     const result2 = {
         "message": null,
         //"url": "https://punchout.cloud/oci/cart-viewer",
         'url': `${process.env.OCI_FORM_ACTION_URL}/${punchoutId}`,
-        "data_array": [
-            { "key": "NEW_ITEM-CURRENCY[2]", "value": "EUR", },
-            { "key": "NEW_ITEM-QUANTITY[3]", "value": "1" },
-            { "key": "NEW_ITEM-VENDORMAT[2]", "value": "" },
-            { "key": "NEW_ITEM-DESCRIPTION[3]", "value": "Plate,filler" },
-            { "key": "NEW_ITEM-PRICE[2]", "value": "0" },
-            { "key": "NEW_ITEM-PRICEUNIT[3]", "value": "1" },
-            { "key": "NEW_ITEM-VENDORMAT[3]", "value": "" },
-            { "key": "NEW_ITEM-QUANTITY[2]", "value": "12" },
-            { "key": "NEW_ITEM-VENDORMAT[1]", "value": "" },
-            { "key": "NEW_ITEM-DESCRIPTION[1]", "value": "Gearbox, 6S1611BO+INT3" },
-            { "key": "NEW_ITEM-QUANTITY[1]", "value": "1" },
-            { "key": "NEW_ITEM-PRICEUNIT[1]", "value": "1" },
-            { "key": "NEW_ITEM-MATGROUP[1]", "value": "" },
-            { "key": "NEW_ITEM-PRICE[1]", "value": "0" },
-            { "key": "NEW_ITEM-MANUFACTMAT[2]", "value": "" },
-            { "key": "NEW_ITEM-LEADTIME[3]", "value": "" },
-            { "key": "NEW_ITEM-LEADTIME[1]", "value": "" },
-            { "key": "NEW_ITEM-CURRENCY[1]", "value": "EUR" },
-            { "key": "NEW_ITEM-LEADTIME[2]", "value": "" },
-            { "key": "NEW_ITEM-UNIT[1]", "value": "EA" },
-            { "key": "NEW_ITEM-MANUFACTMAT[1]", "value": "" },
-            { "key": "NEW_ITEM-MATGROUP[2]", "value": "" },
-            { "key": "NEW_ITEM-UNIT[3]", "value": "EA" },
-            { "key": "NEW_ITEM-MANUFACTMAT[3]", "value": "" },
-            { "key": "NEW_ITEM-DESCRIPTION[2]", "value": "Bolt,flange, M10X70 DAF9257" },
-            { "key": "NEW_ITEM-MATGROUP[3]", "value": "" },
-            { "key": "NEW_ITEM-PRICE[3]", "value": "0" },
-            { "key": "NEW_ITEM-UNIT[2]", "value": "EA" },
-            { "key": "NEW_ITEM-CURRENCY[3]", "value": "EUR" },
-            { "key": "NEW_ITEM-PRICEUNIT[2]", "value": "1" }
-        ],
+        "data_array": items,
         "method": null,
         "action": null
     }
